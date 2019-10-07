36e0943a7dcef767819cbc67ef1561ea2c02dca0
Gabriel Fumelli <gabriel.fumelli@gmail.com>
2019-10-07 16:59:20 -0300

    configurando conexao com o banco de dados 3

--- src/main/java/com/euvin/euvin/data/DataConfiguration.java
+++ src/main/java/com/euvin/euvin/data/DataConfiguration.java
@@ -4,11 +4,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.hibernate.dialect.Database;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration{
@@ -28,15 +25,4 @@ public class DataConfiguration{
 
         return basicDataSource;
     }
-    
-    @Bean
-	public JpaVendorAdapter jpaVendorAdapter(){
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.POSTGRESQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-		adapter.setPrepareConnection(true);
-		return adapter;
-	}
 }
